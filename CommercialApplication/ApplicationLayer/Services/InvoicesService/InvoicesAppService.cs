8880acd6df93c0b6e3d676f26c6d2826b0eea545
{Korisnik@DESKTOP-F9LA78M} <{Korisnik@DESKTOP-F9LA78M}>
2020-01-07 23:01:14 +0100

    order set to close state

--- CommercialApplication/ApplicationLayer/Services/InvoicesService/InvoicesAppService.cs
+++ CommercialApplication/ApplicationLayer/Services/InvoicesService/InvoicesAppService.cs
@@ -40,8 +40,6 @@ namespace CommercialApplicationCommand.ApplicationLayer.Services.InvoicesService
                 Customer customer = this.invoiceCustomerService.SelectByInvoiceId(connection, invoice.Id);
 
                 Order order = this.orderService.SelectById(connection, invoice.OrderId);
-                order.State = new State("Close");
-                this.orderService.Update(connection, order);
 
                 return new InvoiceDto
                 {
@@ -106,6 +104,11 @@ namespace CommercialApplicationCommand.ApplicationLayer.Services.InvoicesService
                         IEnumerable<InvoiceItem> calculatedInvoiceItemsWithBasicAndActionDiscount = this.invoiceItemService.IncludeActionDiscountForPaying(connection, invoiceItems);
                         this.invoiceItemService.InsertList(connection, calculatedInvoiceItemsWithBasicAndActionDiscount, transaction);
                         this.invoiceItemInvoicesService.InsertList(connection, calculatedInvoiceItemsWithBasicDiscount, invoiceId, transaction);
+
+                        Order order = this.orderService.SelectById(connection, invoice.OrderId);
+                        order.State = new State("Close");
+                        this.orderService.Update(connection, order);
+
                         transaction.Commit();
                     }
                     catch (Exception ex)
