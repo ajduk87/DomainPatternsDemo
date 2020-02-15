f4cf004fd3eac8d5a5982401685896d1ddf04e55
{Korisnik@DESKTOP-F9LA78M} <{Korisnik@DESKTOP-F9LA78M}>
2020-02-15 21:59:33 +0100

    finished frontent part for client application

--- CommercialApplication/ApplicationLayer/Models/Action/ActionCreateModel.cs
+++ CommercialApplication/ApplicationLayer/Models/Action/ActionCreateModel.cs
@@ -5,6 +5,5 @@
         public long ProductId { get; set; }
         public double Discount { get; set; }
         public int ThresholdAmount { get; set; }
-        public long CustomerId { get; set; }
     }
 }
