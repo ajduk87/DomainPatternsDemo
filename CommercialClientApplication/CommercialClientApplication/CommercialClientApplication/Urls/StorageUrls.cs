e6ed5a56b5b73987913d1a132a4a647af22c9702
nikola.ajdukovic <najduk@merann.ru>
2020-02-18 11:43:07 +0100

    storage control are started to implementation

---
+++ CommercialClientApplication/CommercialClientApplication/CommercialClientApplication/Urls/StorageUrls.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommercialClientApplication.Urls
+{
+    public class StorageUrls : Urls
+    {
+        public string Storage;
+
+        public StorageUrls()
+        {
+            this.Storage = $"{ServerIpAddress}/api/storage";
+        }
+    }
+}
