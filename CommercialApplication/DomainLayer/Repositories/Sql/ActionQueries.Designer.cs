c3a26402b60c7109f6016b67f39ab98df8bee636
mpm94 <mpm1628@gmail.com>
2020-02-24 20:45:44 +0100

    queries are changed

--- CommercialApplication/DomainLayer/Repositories/Sql/ActionQueries.Designer.cs
+++ CommercialApplication/DomainLayer/Repositories/Sql/ActionQueries.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CommercialApplicationCommand.DomainLayer.Repositories.Sql {
+namespace CommercialApplication.DomainLayer.Repositories.Sql {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace CommercialApplicationCommand.DomainLayer.Repositories.Sql {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CommercialApplicationCommand.DomainLayer.Repositories.Sql.ActionQueries", typeof(ActionQueries).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CommercialApplication.DomainLayer.Repositories.Sql.ActionQueries", typeof(ActionQueries).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- CommercialApplication/DomainLayer/Repositories/Sql/OrderItemOrderQueries.Designer.cs
+++ CommercialApplication/DomainLayer/Repositories/Sql/OrderItemOrderQueries.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CommercialApplicationCommand.DomainLayer.Repositories.Sql {
+namespace CommercialApplication.DomainLayer.Repositories.Sql {
     using System;
     
     
@@ -19,7 +19,7 @@ namespace CommercialApplicationCommand.DomainLayer.Repositories.Sql {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class OrderItemOrderQueries {
@@ -39,7 +39,7 @@ namespace CommercialApplicationCommand.DomainLayer.Repositories.Sql {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CommercialApplicationCommand.DomainLayer.Repositories.Sql.OrderItemOrderQueries", typeof(OrderItemOrderQueries).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CommercialApplication.DomainLayer.Repositories.Sql.OrderItemOrderQueries", typeof(OrderItemOrderQueries).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
